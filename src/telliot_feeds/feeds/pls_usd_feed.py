be4a091d0beb845e38656be53c411ba15406b070
EmanuellQA <emanuel@liquidloans.io>
2024-11-30 19:31:04 -0300

    updated pulseXSource, plsNfetch feeds for mainnet

--- src/telliot_feeds/feeds/pls_usd_feed.py
+++ src/telliot_feeds/feeds/pls_usd_feed.py
@@ -6,18 +6,34 @@ from telliot_feeds.sources.price.spot.coinpaprika import CoinpaprikaSpotPriceSou
 from telliot_feeds.sources.price.spot.uniswapV3 import UniswapV3PriceSource
 from telliot_feeds.sources.price.spot.pulsex_subgraph import PulseXSubgraphSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-pls_usd_median_feed = DataFeed(
-    query=SpotPrice(asset="PLS", currency="USD"),
-    source=PriceAggregator(
-        asset="pls",
-        currency="usd",
-        algorithm="median",
-        sources=[
-            #CoinGeckoSpotPriceSource(asset="pls", currency="usd"),
-            #CoinpaprikaSpotPriceSource(asset="pls-pulsechain", currency="usd"),
-            #UniswapV3PriceSource(asset="pls", currency="usd"),
-            PulseXSubgraphSource(asset="wpls", currency="usd"), #testnet wpls. Check pulsex_subgraph.py to confirm token address
-        ],
-    ),
-)
+if os.getenv("NETWORK_ID") == "943":
+    pls_usd_median_feed = DataFeed(
+        query=SpotPrice(asset="PLS", currency="USD"),
+        source=PriceAggregator(
+            asset="pls",
+            currency="usd",
+            algorithm="median",
+            sources=[
+                PulseXSubgraphSource(asset="wpls", currency="usd"), 
+            ],
+        ),
+    )
+else:
+    pls_usd_median_feed = DataFeed(
+        query=SpotPrice(asset="PLS", currency="USD"),
+        source=PriceAggregator(
+            asset="pls",
+            currency="usd",
+            algorithm="median",
+            sources=[
+                CoinGeckoSpotPriceSource(asset="pls", currency="usd"),
+                CoinpaprikaSpotPriceSource(asset="pls-pulsechain", currency="usd"),
+                UniswapV3PriceSource(asset="pls", currency="usd"),
+                PulseXSubgraphSource(asset="wpls", currency="usd"), 
+            ],
+        ),
+    )
