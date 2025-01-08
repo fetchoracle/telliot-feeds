fb50c56d7e357b4cbd7a74f5c7bd4565eecf5db6
EmanuellQA <emanuel@liquidloans.io>
2025-01-08 18:09:03 -0300

    Updated to handle testnet feeds and subgraph urls without .env vars

--- src/telliot_feeds/sources/price/spot/pulsex_subgraph.py
+++ src/telliot_feeds/sources/price/spot/pulsex_subgraph.py
@@ -1,9 +1,11 @@
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 from dataclasses import dataclass
 from dataclasses import field
 from typing import Any
+
+from decimal import Decimal
 
 import requests
 
@@ -15,27 +17,8 @@ from telliot_feeds.utils.log import get_logger
 
 logger = get_logger(__name__)
 
-load_dotenv()
-network_id = os.getenv("NETWORK_ID")
-
-testnet_graph = {
-    "pulsex": "https://graph.v4.testnet.pulsechain.com",
-}
-testnet_tokens = {
-    "dai": "0x826e4e896cc2f5b371cd7bb0bd929db3e3db67c0",
-    "usdc": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-    "plsx": "0x8a810ea8b121d08342e9e7696f4a9915cbe494b7",
-    "fetch": "0xC0573e2Fc47B26fb05097a553BBfcf0166bada0A",
-    "wpls": "0x70499adebb11efd915e3b69e700c331778628707",
-    "hex": "0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39",
-    "inc": "0x6eFAfcb715F385c71d8AF763E8478FeEA6faDF63",
-    "loan": "0x2720F69787cE6ba408fB6e2282d7640E805DF367",
-}
-
-mainnet_graph = {
-    "pulsex": "https://graph.pulsechain.com",
-}
-mainnet_tokens = {
+pulsex_subgraph_supporten_tokens = {
+#mainnet tokens
     "wpls": "0xa1077a294dde1b09bb078844df40758a5d0f9a27",
     "dai": "0xefd766ccb38eaf1dfd701853bfce31359239f305",
     "usdc": "0x15d38573d2feeb82e7ad5187ab8c1d52810b1f07",
@@ -44,23 +27,26 @@ mainnet_tokens = {
     "hex": "0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39",
     "inc": "0x2fa878ab3f87cc1c9737fc071108f904c0b0c95d",
     "loan": "0x9159f1d2a9f51998fc9ab03fbd8f265ab14a1b3b",
+#Testnet Tokens
+    "t*dai": "0x826e4e896cc2f5b371cd7bb0bd929db3e3db67c0",
+    "t*usdc": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    "t*plsx": "0x8a810ea8b121d08342e9e7696f4a9915cbe494b7",
+    "t*fetch": "0xC0573e2Fc47B26fb05097a553BBfcf0166bada0A",
+    "t*wpls": "0x70499adebb11efd915e3b69e700c331778628707",
+    "t*hex": "0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39",
+    "t*inc": "0x6eFAfcb715F385c71d8AF763E8478FeEA6faDF63",
+    "t*loan": "0x2720F69787cE6ba408fB6e2282d7640E805DF367",
 }
-
-if network_id == "369":  # mainnet
-    pulsex_subgraph_supporten_tokens, url = mainnet_tokens, mainnet_graph
-elif network_id == "943":  # testnet
-    pulsex_subgraph_supporten_tokens, url = testnet_tokens, testnet_graph
-else:
-    raise ValueError("Unsupported pulsexSubgraph NETWORK_ID. Check .env")
-
-
+TESTNET_GRAPH = "https://graph.v4.testnet.pulsechain.com"
+MAINNET_GRAPH = "https://graph.pulsechain.com"
 
 class PulseXSubgraphService(WebPriceService):
-    """PulseX Subgraph Price Service for token price"""
+    """PulseX Subgraph Price Service for token price.
+    Checks if the asset is testnet or mainnet and uses the correct url to fetch prices"""
 
-    def __init__(self, **kwargs: Any) -> None:
+    def __init__(self, **kwargs: Any) -> None:  
         kwargs["name"] = "PulseX Subgraph Price Service"
-        kwargs["url"] = url["pulsex"]
+        kwargs["url"] = None
         kwargs["timeout"] = 10.0
         super().__init__(**kwargs)
 
@@ -70,7 +56,9 @@ class PulseXSubgraphService(WebPriceService):
         This implementation gets the price from the PulseX hosted subgraphs
 
         """
-
+        self.url = TESTNET_GRAPH if asset.startswith('t*') else MAINNET_GRAPH
+        logger.info(f'Using {self.url} to fetch values')
+        
         asset = asset.lower()
         currency = currency.lower()
 
