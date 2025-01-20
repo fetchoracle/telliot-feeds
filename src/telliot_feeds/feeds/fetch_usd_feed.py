1bec1cd8fcd0a26ce08352236ec57c93f38970cc
EmanuellQA <emanuel@liquidloans.io>
2025-01-20 17:13:44 -0300

    Updated fetch-usd-spot for pulseXv2
    After deploying the liquidity pool to pulseX V2 we had to add pulseX v2 source to get the FETCH price

--- src/telliot_feeds/feeds/fetch_usd_feed.py
+++ src/telliot_feeds/feeds/fetch_usd_feed.py
@@ -2,7 +2,7 @@
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
 from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
-from telliot_feeds.sources.price.spot.pulsex_subgraph import PulseXSubgraphSource
+from telliot_feeds.sources.price.spot.pulsex_subgraph_v2 import PulseXSubgraphv2Source
 from telliot_feeds.sources.price_aggregator import PriceAggregator
 
 fetch_usd_median_feed = DataFeed(
@@ -11,6 +11,6 @@ fetch_usd_median_feed = DataFeed(
         asset="fetch",
         currency="usd",
         algorithm="median",
-        sources=[PulseXSubgraphSource(asset="fetch", currency="usd")],
+        sources=[PulseXSubgraphv2Source(asset="fetch", currency="usd")],
     )
 )
