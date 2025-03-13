acf34fde75f63b611791e88abfabc204073537a9
emanuel <emanuell.cp@gmail.com>
2025-03-13 18:04:29 -0300

    Updated fetch feed to use 9inchRPC and only 9inch pool

--- src/telliot_feeds/feeds/fetch_usd_feed.py
+++ src/telliot_feeds/feeds/fetch_usd_feed.py
@@ -1,8 +1,8 @@
 """Datafeed for current price of FETCH in USD."""
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
-from telliot_feeds.sources.price.spot.pulsex_subgraph_v2 import PulseXSubgraphv2Source
 from telliot_feeds.sources.price.spot.dexscreener_api import DexScreenerApiSource
+from telliot_feeds.sources.price.spot.nineinch_v3_rpc import NineInchV3Source
 from telliot_feeds.sources.price_aggregator import PriceAggregator
 
 fetch_usd_median_feed = DataFeed(
@@ -12,10 +12,8 @@ fetch_usd_median_feed = DataFeed(
         currency="usd",
         algorithm="median",
         sources=[
-            PulseXSubgraphv2Source(asset="fetch", currency="usd"),
             DexScreenerApiSource(asset="369,9inch,fetch", currency="usdl"),
-            DexScreenerApiSource(asset="369,9mm,fetch", currency="wpls"),
-            DexScreenerApiSource(asset="369,pulsex,fetch", currency="wpls"),
-            ],
+            NineInchV3Source(asset="fetch", currency="usdl"),
+        ],
     )
 )
