866d4e94be75e2eaf9cbdbf9f2ac214542823b40
EmanuellQA <emanuel@liquidloans.com>
2025-03-24 12:10:06 -0300

    better error handling when no data returned

--- src/telliot_feeds/sources/price/spot/pulsex_subgraph.py
+++ src/telliot_feeds/sources/price/spot/pulsex_subgraph.py
@@ -106,19 +106,29 @@ class PulseXSubgraphService(WebPriceService):
         elif "response" in data:
             response = data["response"]
 
+            logger.debug(f"PulseX Subgraph Response: {response}")
+            if "errors" in response:
+                logger.error(f"PulseX Subgraph Error: {response['errors']}")
+                return None, None
+
             try:
-                if response["data"]["token"] == None:
+
+                token_data = response.get("data", {}).get("token")
+
+                if token_data is None:
                     logger.error(f"No data found for the token {token}")
                     logger.info(f"It is possible that no Liquidity Pool exists including this token ({token})")
                     return None, None
 
-                price = float(response["data"]["token"]["derivedUSD"])
+                price = float(token_data["derivedUSD"])
                 return price, datetime_now_utc()
             except KeyError as e:
                 msg = f"Error parsing Pulsechain Subgraph response: KeyError: {e}"
-                if response["data"]["token"] == None:
-                    msg = f"Invalid token address: {token}"
-                logger.critical(msg)
+                logger.error(msg)
+                return None, None
+            except ValueError as e:
+                msg = f"Error converting 'derivedUSD' to float: {e}"
+                logger.error(msg)
                 return None, None
 
         else:
