60335560879ab74ed12219d199c4ad120a3e05c2
EmanuellQA <emanuel@liquidloans.com>
2025-03-27 18:27:09 -0300

    Added pulseXRPC and Dexscreener API sources

--- src/telliot_feeds/feeds/plsx_usd_feed.py
+++ src/telliot_feeds/feeds/plsx_usd_feed.py
@@ -1,6 +1,8 @@
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
 from telliot_feeds.sources.price.spot.pulsex_subgraph import PulseXSubgraphSource
+from telliot_feeds.sources.price.spot.pulsex_rpc import PulseXRPC
+from telliot_feeds.sources.price.spot.dexscreener_api import DexScreenerApiSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
 
 plsx_usd_median_feed = DataFeed(
@@ -11,6 +13,8 @@ plsx_usd_median_feed = DataFeed(
         algorithm="median",
         sources=[
             PulseXSubgraphSource(asset="plsx", currency="usd"),
+            PulseXRPC(asset="plsx", currency="dai"),
+            DexScreenerApiSource(asset="369,pulsex,plsx", currency="wpls"),
         ],
     ),
 )
