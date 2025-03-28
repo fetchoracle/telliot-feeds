ce756ff196aba57fcd53cb685907011386349e14
EmanuellQA <emanuel@liquidloans.com>
2025-03-27 23:09:25 -0300

    Updated it to take more assets, not only fetch

--- src/telliot_feeds/sources/price/spot/nineinch_v3_rpc.py
+++ src/telliot_feeds/sources/price/spot/nineinch_v3_rpc.py
@@ -74,6 +74,10 @@ TOKEN_ABI = """
 ]
 """
 
+supported_pools = {
+    "fetch/usdl": "0xf3dA9A1FF38c6D774e6aA583302A5aB7646b7025",
+}
+
 class PairPriceService(WebPriceService):
     """9inch V3 RPC Price Service.
         Tries to fetch the asset price from a pool.
@@ -84,14 +88,21 @@ class PairPriceService(WebPriceService):
     def __init__(self, **kwargs: Any) -> None:
         kwargs["name"] = "9inch V3 price service"
         kwargs["url"] = "https://rpc.pulsechain.com"
-        self.pair_address = Web3.toChecksumAddress("0xf3dA9A1FF38c6D774e6aA583302A5aB7646b7025")
+        self.pair_address = None
         super().__init__(**kwargs)
 
     async def get_price(self, asset: str, currency: str) -> OptionalDataPoint[float]:
         try:
             w3 = Web3(Web3.HTTPProvider(self.url))
+
+            if asset + '/' + currency in supported_pools:
+                self.pair_address = supported_pools.get(asset+'/'+currency)
+            else:
+                logger.error(f"Asset: {asset} and Currency: {currency} not in supported pools list")
+                return None, None
+
             pool_contract = w3.eth.contract(address=self.pair_address, abi=POOL_ABI)
-            logger.info(f"Fetching {asset}/{currency} in {self.pair_address} pool")
+            logger.debug(f"Fetching {asset}/{currency} in {self.pair_address} pool")
 
             token0_address = pool_contract.functions.token0().call()
             token1_address = pool_contract.functions.token1().call()
@@ -99,8 +110,8 @@ class PairPriceService(WebPriceService):
             logger.debug(f"Token0 address: {token0_address}")
             logger.debug(f"Token1 address: {token1_address}")
 
-            token0_contract = w3.eth.contract(address=token1_address, abi=TOKEN_ABI)
-            token1_contract = w3.eth.contract(address=token0_address, abi=TOKEN_ABI)
+            token0_contract = w3.eth.contract(address=token0_address, abi=TOKEN_ABI)
+            token1_contract = w3.eth.contract(address=token1_address, abi=TOKEN_ABI)
 
             token0_decimals = token0_contract.functions.decimals().call()
             token0_symbol = token0_contract.functions.symbol().call()
@@ -121,19 +132,18 @@ class PairPriceService(WebPriceService):
             sqrt_price_x96 = slot0_data[0]
             logger.debug(f'sqrt_price_x96: {slot0_data[0]}')
 
-            # Determine if asset is token0 or token1 to calculate correct asset price
             if token0_symbol.lower() == asset.lower():
-                # token0 is the asset, so token1 is the currency (USDL)
+                # token0 is the asset, so token1 is the currency
                 if token1_symbol.lower() == currency.lower():
-                    price = ((2 ** 96) / sqrt_price_x96) ** 2 * (10 ** (token0_decimals - token1_decimals))
+                    price = (sqrt_price_x96 / (2 ** 96)) ** 2 * (10 ** (token1_decimals - token0_decimals))
                     logger.debug(f"Price of {asset} in {currency}: {price}")
                 else:
                     logger.error(f"Currency {currency} not found in the pool")
                     return None, None
             elif token1_symbol.lower() == asset.lower():
-                # token1 is the asset, so token0 is the currency (USDL)
+                # token1 is the asset, so token0 is the currency
                 if token0_symbol.lower() == currency.lower():
-                    price = (sqrt_price_x96 / (2 ** 96)) ** 2 * (10 ** (token1_decimals - token0_decimals))
+                    price = ((2 ** 96) / sqrt_price_x96) ** 2 * (10 ** (token0_decimals - token1_decimals))
                     logger.debug(f"Price of {asset} in {currency}: {price}")
                 else:
                     logger.error(f"Currency {currency} not found in the pool")
@@ -142,7 +152,7 @@ class PairPriceService(WebPriceService):
                 logger.error(f"Asset {asset} not found in the pool {self.pair_address}")
                 return None, None
 
-            logger.debug(f"Price of {asset} in {currency}: {price}")
+            logger.info(f"Price of {asset} in {currency}: {price}")
             timestamp = datetime.fromtimestamp(timestamp)
             return float(price), timestamp
         except Exception as e:
