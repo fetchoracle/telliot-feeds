f1f97dd8ed3f07550925e6c77d77b8e085e3e004
EmanuellQA <emanuel@liquidloans.com>
2025-04-08 16:32:08 -0300

    updated candidate sources

--- src/telliot_feeds/feeds/plstest_usd_feed.py
+++ src/telliot_feeds/feeds/plstest_usd_feed.py
@@ -15,12 +15,9 @@ plstest_usd_median_feed = DataFeed(
             PulseXRPC(asset="wpls", currency="dai"),
             PulseXRPC(asset="wpls", currency="usdc"),
             PulseXRPC(asset="wpls", currency="usdt"),
-            DexScreenerApiSource(asset="369,pulsex,wpls", currency="dai"),
+            PulseXRPC(asset="wpls2", currency="dai"),
             DexScreenerApiSource(asset="369,pulsexv2,wpls", currency="dai"),
-            DexScreenerApiSource(asset="369,pulsex,wpls", currency="usdc"),
-            DexScreenerApiSource(asset="369,pulsex,wpls", currency="usdt"),
             DexScreenerApiSource(asset="369,pulsexv2,wpls", currency="plsx"),
-            DexScreenerApiSource(asset="369,9inch,wpls", currency="plsx"),
         ],
     ),
 )
